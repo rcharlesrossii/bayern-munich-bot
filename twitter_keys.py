4687d248537b669026e64da54e1e9305c25213bf
Charlie Ross <rcharlesrossii@gmail.com>
2024-08-03 15:56:19 -0500

    Delete twitter_keys.py
    THIS SHOULD NOT HAVE BEEN PUBLIC OOPSIES

--- twitter_keys.py
+++
@@ -1,7 +0,0 @@
-api_key = "NtnCsXrIndcY2W55C5un6Am7i"
-api_key_secret = "X5HIJzJkQbeDRu8P7QP6U1B47jDy0II2eRZFHnn5L1FDeSlKjt"
-access_token = "1694024029227585536-88AnmBrZqFBxlzimdWg6ChTIrmX65t"
-access_token_secret = "BibtTDXSU9dX5JP6Uy5cj3faAUhFdm4bOEnZxLETzA03q"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAJ0rpgEAAAAAztON1velmG0SjS2Da3Y2J7nR8AY%3DqieJqlVogH5gd7KtQSO9HqBAvtTOjJq1e5ingQNarH9IDIvzZ8"
-client_token = "ejc2Z1BmS1hPU2x0YnRFVEMyejY6MTpjaQ"
-client_secret = "l1FdHUmI-E9PY9i2FtdJalIgU_7Xv3-m-RN_5AVp4_0rY5hDZG"
